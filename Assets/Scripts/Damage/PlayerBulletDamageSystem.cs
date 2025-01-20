a07f81ef37c22bc2d1ce311d48570e29aa2de0f4
Do-then-quit <redfowl@naver.com>
2025-01-20 12:32:31 +0900

    player bullet enemy collision impl.
    총알과 적이 부딪혔을 때의 이벤트에서 조회해서 총알을 없애고 데미지를 주는 코드 작성.

--- Assets/Scripts/Damage/PlayerBulletDamageSystem.cs
+++ Assets/Scripts/Damage/PlayerBulletDamageSystem.cs
@@ -1,61 +1,128 @@
-using System;
-using System.Drawing;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Mathematics;
 using Unity.Physics;
-using Unity.Rendering;
-using Unity.Transforms;
+using Unity.Physics.Systems;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
-public partial struct PlayerBulletDamageSystem : ISystem
+[RequireMatchingQueriesForUpdate]
+[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+[UpdateAfter(typeof(PhysicsSystemGroup))]
+public partial struct BulletCollisionSystem : ISystem
 {
+    internal ComponentDataHandles m_ComponentDataHandles;
     
-    //public Action<int, float3, float4> OnBulletDamage;
-
-    public void OnUpdate(ref SystemState state)
+    internal struct ComponentDataHandles
     {
-        SystemAPI.GetComponentLookup<HealthComponent>();
+        public ComponentLookup<DamageComponent> BulletDataLookup;
+        public ComponentLookup<HealthComponent> EnemyHealthLookup;
+        
+        public ComponentLookup<PlayerBullet> PlayerBulletLookup;
+        public ComponentLookup<Enemy> EnemyTagLookup;
+
+        public ComponentDataHandles(ref SystemState systemState)
+        {
+            BulletDataLookup = systemState.GetComponentLookup<DamageComponent>(true);
+            EnemyHealthLookup = systemState.GetComponentLookup<HealthComponent>(false);
+            
+            PlayerBulletLookup = systemState.GetComponentLookup<PlayerBullet>(true);
+            EnemyTagLookup = systemState.GetComponentLookup<Enemy>(true);
+        }
+
+        public void Update(ref SystemState systemState)
+        {
+            BulletDataLookup.Update(ref systemState);
+            EnemyHealthLookup.Update(ref systemState);
+            
+            PlayerBulletLookup.Update(ref systemState);
+            EnemyTagLookup.Update(ref systemState);
+        }
     }
 
-    struct BulletEnemyCollideJob : ICollisionEventsJob
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
     {
-        [ReadOnly] public ComponentLookup<PlayerBullet> BulletLookup;
-        [ReadOnly] public ComponentLookup<Enemy> EnemiesLookup;
-        [ReadOnly] public ComponentLookup<DamageComponent> DamageComponentLookup;
-        public ComponentLookup<HealthComponent> HealthComponentLookup;
-        public EntityCommandBuffer ECB;
+        // state.RequireForUpdate(state.GetEntityQuery(
+        //     ComponentType.ReadWrite<DamageComponent>(),
+        //     ComponentType.ReadOnly<HealthComponent>()
+        //     ));
+        state.RequireForUpdate(state.GetEntityQuery(ComponentType.ReadOnly<PlayerBullet>()));
+        state.RequireForUpdate(state.GetEntityQuery(ComponentType.ReadOnly<Enemy>()));
+
+        state.RequireForUpdate<SimulationSingleton>();
+        m_ComponentDataHandles = new ComponentDataHandles(ref state);
+
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        m_ComponentDataHandles.Update(ref state);
+        
+        EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.TempJob);
+
+        var simulation = SystemAPI.GetSingleton<SimulationSingleton>();
+        state.Dependency = new BulletCollisionEventJob
+        {
+            BulletDataLookup = m_ComponentDataHandles.BulletDataLookup,
+            EnemyHealthLookup = m_ComponentDataHandles.EnemyHealthLookup,
+            PlayerBulletLookup = m_ComponentDataHandles.PlayerBulletLookup,
+            EnemyTagLookup = m_ComponentDataHandles.EnemyTagLookup,
+            entityCommandBuffer = ecb
+        }.Schedule(simulation, state.Dependency);
+        state.Dependency.Complete();
+        ecb.Playback(state.EntityManager);
+        ecb.Dispose();
+    }
+
+    [BurstCompile]
+    struct BulletCollisionEventJob : ICollisionEventsJob
+    {
+        [ReadOnly] public ComponentLookup<DamageComponent> BulletDataLookup;
+        public ComponentLookup<HealthComponent> EnemyHealthLookup;
+        
+        [ReadOnly] public ComponentLookup<PlayerBullet> PlayerBulletLookup;
+        [ReadOnly] public ComponentLookup<Enemy> EnemyTagLookup;
+        
+        public EntityCommandBuffer entityCommandBuffer;
+
         public void Execute(CollisionEvent collisionEvent)
         {
-            var entityA = collisionEvent.EntityA;
-            var entityB = collisionEvent.EntityB;
-            
-            bool isBulletA = BulletLookup.HasComponent(entityA);
-            bool isBulletB = BulletLookup.HasComponent(entityB);
-            bool isEnemyA = EnemiesLookup.HasComponent(entityA);
-            bool isEnemyB = EnemiesLookup.HasComponent(entityB);
-            
-            if ((isBulletA && isEnemyB) || (isBulletB && isEnemyA))
-            {
-                var bullet = isBulletA ? entityA : entityB;
-                var enemy = isEnemyA ? entityA : entityB;
-                
-                
-                
-                // 적에게 데미지 주기
-                if (HealthComponentLookup.HasComponent(enemy))
-                {
-                    var health = HealthComponentLookup[enemy];
-                    health.CurrentHealth -= 10; // 탄환 데미지 예시
-                    HealthComponentLookup[enemy] = health;
-                }
+            Entity entityA = collisionEvent.EntityA;
+            Entity entityB = collisionEvent.EntityB;
 
-                // 탄환 삭제하기
-                //DamageUIController.OnBulletDamage?.Invoke((int)bulletDamage.ValueRO.Damage, enemyPosition, bulletColor.ValueRO.Value);
+            bool isEntityABullet = BulletDataLookup.HasComponent(entityA) && PlayerBulletLookup.HasComponent(entityA);
+            bool isEntityBBullet = BulletDataLookup.HasComponent(entityB) && PlayerBulletLookup.HasComponent(entityB);
+
+            bool isEntityAEnemy = EnemyHealthLookup.HasComponent(entityA) && EnemyTagLookup.HasComponent(entityA);
+            bool isEntityBEnemy = EnemyHealthLookup.HasComponent(entityB) && EnemyTagLookup.HasComponent(entityB);
+
+            if (isEntityABullet && isEntityBEnemy)
+            {
+                ApplyDamage(entityA, entityB);
             }
-            // damage event
-            //DamageUIController.OnBulletDamage?.Invoke((int)bulletDamage.ValueRO.Damage, enemyPosition, bulletColor.ValueRO.Value);
+            else if (isEntityBBullet && isEntityAEnemy)
+            {
+                ApplyDamage(entityB, entityA);
+            }
             
+            
+        }
+
+        private void ApplyDamage(Entity bullet, Entity enemy)
+        {
+            var bulletDamage = BulletDataLookup[bullet];
+            var enemyHealth = EnemyHealthLookup[enemy];
+
+            // Apply damage to the enemy
+            enemyHealth.CurrentHealth -= bulletDamage.Damage;
+            EnemyHealthLookup[enemy] = enemyHealth;
+
+
+            // Destroy the bullet
+            entityCommandBuffer.DestroyEntity(bullet);
         }
     }
 }
+
